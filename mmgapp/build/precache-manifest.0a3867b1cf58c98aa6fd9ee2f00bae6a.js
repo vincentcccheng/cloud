2e161db4e08d519f018a6f006b980a6bc0fe8c0d
Vincent Cheng <nf1vcc@mmgoverseas.federated.fds>
2021-06-11 23:03:28 +0800

    release 1.6

---
+++ mmgapp/build/precache-manifest.0a3867b1cf58c98aa6fd9ee2f00bae6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7884aceddffd7534b672fa93a620d438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3df8004e2ccffc759411",
+    "url": "/static/css/2.adcc7399.chunk.css"
+  },
+  {
+    "revision": "d365621b48bda1ef9a72",
+    "url": "/static/css/main.d81c3ca9.chunk.css"
+  },
+  {
+    "revision": "3df8004e2ccffc759411",
+    "url": "/static/js/2.a33824cb.chunk.js"
+  },
+  {
+    "revision": "d365621b48bda1ef9a72",
+    "url": "/static/js/main.1a529be2.chunk.js"
+  },
+  {
+    "revision": "e8ae3a5e8133ca2ba308",
+    "url": "/static/js/runtime~main.25c39bf2.js"
+  }
+]);
